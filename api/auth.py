d3fc3a7d1bb498c7c6ffef0c9758634ff583c822
Marcelo Santos <a79433@ualg.pt>
2025-05-01 18:40:03 +0100

    Sistema de autendicação API + Flutter + Token + Google Auth Implementado com sucesso

--- api/auth.py
+++ api/auth.py
@@ -5,9 +5,31 @@ from datetime import datetime, timedelta
 import jwt
 
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
 INTERNAL_SECRET = os.getenv("INTERNAL_SECRET", "dev-secret")
-ALLOWED_STUDENT_DOMAIN = "alunos.ualg.pt"
-ALLOWED_STAFF_DOMAIN = "ualg.pt"
+ALLOWED_STUDENT_DOMAIN = "gmail.com"
+ALLOWED_STAFF_DOMAIN = "gmail.com"
+REDIRECT_URI = "http://localhost:8000/callback"
+
+def exchange_code_for_token(code: str) -> str:
+    response = requests.post("https://oauth2.googleapis.com/token", data={
+        "code": code,
+        "client_id": GOOGLE_CLIENT_ID,
+        "client_secret": GOOGLE_CLIENT_SECRET,
+        "redirect_uri": REDIRECT_URI,
+        "grant_type": "authorization_code"
+    })
+
+    print("Google OAuth token response:", response.json())
+
+    if response.status_code != 200:
+        raise HTTPException(status_code=401, detail="Failed to exchange code for token")
+
+    id_token = response.json().get("id_token")
+    if not id_token:
+        raise HTTPException(status_code=401, detail="Missing id_token")
+
+    return id_token
 
 def verify_google_token(id_token: str) -> dict:
     response = requests.get(
@@ -23,9 +45,11 @@ def verify_google_token(id_token: str) -> dict:
         raise HTTPException(status_code=401, detail="Invalid client ID")
 
     email = payload.get("email", "")
-    if email.endswith(ALLOWED_STUDENT_DOMAIN):
+    domain = email.split('@')[-1]
+    
+    if domain == ALLOWED_STUDENT_DOMAIN:
         role = "student"
-    elif email.endswith(ALLOWED_STAFF_DOMAIN):
+    elif domain == ALLOWED_STAFF_DOMAIN:
         role = "staff"
     else:
         raise HTTPException(status_code=403, detail="Unauthorized domain")
